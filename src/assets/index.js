757aeccec340b73fd41984973963eb0dbc85f306
Virgil Hawkins <munizalex45@gmail.com>
2023-05-04 20:07:34 -0300

    Gestor Kanban

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+const assets = {
+    images: {
+        logoDark: require('./images/logo-dark.png'),
+        logoLight: require('./images/logo-light.png'),
+        appIcon: require('./images/favicon.png')
+    },
+    colors: {
+        secondary: '#292929',
+    }   
+}
+
+export default assets;
